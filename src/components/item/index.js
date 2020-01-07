5935917c414deb990a3530282d8efdde9d9dc647
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-01-06 21:04:21 -0300

    Ajustado layout

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 class Item extends Component {
     constructor(props) {
@@ -9,15 +9,39 @@ class Item extends Component {
     render() {
         const { item } = this.props
         return(
-        <View>
-            <Image style={{height: 100, width: 100}} source={{uri: item.foto}} />
-            <Text>{item.titulo}</Text>
-            <Text>{item.valor}</Text>
-            <Text>{item.local_anuncio}</Text>
-            <Text>{item.data_publicacao}</Text>
+        <View style={styles.item}>
+            <View>
+                <Image style={{height: 100, width: 100}} source={{uri: item.foto}} />
+            </View>
+            <View style={styles.descricao}>
+                <Text style={styles.titulo}>{item.titulo}</Text>
+                <Text>R$ {item.valor}</Text>
+                <Text>{item.local_anuncio}</Text>
+                <Text>{item.data_publicacao}</Text>
+            </View>
         </View>
         )
     }
 }
 
+const styles = StyleSheet.create({
+    item: {
+        borderWidth: .5,
+        borderColor: '#999',
+        margin: 10,
+        padding: 10,
+        flexDirection: 'row',
+        borderRadius: 10,
+        backgroundColor: '#fff'
+    },
+    descricao: {
+        marginLeft: 10
+    },
+    titulo: {
+        color: '#453',
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginBottom: 5
+    }
+})
 export default Item;
